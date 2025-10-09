0fff430bafbb30afed72d80591850e06cb8206c3
piztanza <m.salmanalhafizh@gmail.coma>
2025-10-09 21:47:16 +0700

    try pipeline

--- src/App.tsx
+++ src/App.tsx
@@ -88,7 +88,7 @@ export default function App() {
           </div>
         </div>
 
-        {/* Scroll indicator */}
+        {/* Scroll indicator nihh */}
         <div className="absolute bottom-8 left-1/2 -translate-x-1/2 animate-bounce">
           <div className="w-6 h-10 border-2 border-[#3B82F6] rounded-full flex items-start justify-center p-2">
             <div className="w-1 h-2 bg-[#3B82F6] rounded-full"></div>
